2e542c520a605a18e18d296c7e60451bcb9e9e9e
Alexluu13 <alexluu13@gmail.com>
2025-06-11 14:50:13 +0200

    mise a jour config

--- Projet-C#/BookApi/obj/Debug/net9.0/BookApi.AssemblyInfo.cs
+++ Projet-C#/BookApi/obj/Debug/net9.0/BookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204624e825fa37f7f7d77f881efaffd47ef6dcc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b0850eaeb2e0fb47b4a48274520bded1b6c144")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
