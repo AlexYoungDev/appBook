4237921480ddac64e0ca7465a84aff9a95b31664
uvsq22101020 <hadam92@outlook.fr>
2025-06-11 10:30:05 +0200

    creation des models

---
+++ Projet-C#/BookApi/Models/MediaRepository.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class MediaRepository : IRepository<Media>
+{
+    private static List<Media> _medias = new List<Media>
+    {
+        new Ebook { Id = 1, Title = "C# for Beginners", Author = "John Doe", FileFormat = "PDF" },
+        new PaperBook { Id = 2, Title = "ASP.NET Core Guide", Author = "Jane Smith", NumberOfPages = 300 }
+    };
+
+    public Task<IEnumerable<Media>> GetAllAsync()
+    {
+        return Task.FromResult<IEnumerable<Media>>(_medias);
+    }
+
+    public Task<Media?> GetByIdAsync(int id)
+    {
+        var media = _medias.FirstOrDefault(m => m.Id == id);
+        return Task.FromResult<Media?>(media);
+    }
+}
